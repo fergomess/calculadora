4c30b4bbe5382262d6dfa8c99183b84b217163ec
fergomess <fernandagomes769@gmail.com>
2017-08-23 10:57:43 -0300

    Desenvolvimento da Calculadora

---
+++ src/app/app.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+
+import '../assets/css/style.css';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    title: string;
+    calculadora: string;
+    valor: string;
+    sinal : string;
+    total : number;
+    resultado: number;
+    numero: string;
+
+    constructor() {
+        this.title = "CALCULADORA";
+        this.calculadora = '';
+        this.resultado = 0;
+        this.valor = '';
+        this.numero = '';
+    }
+
+    ngOnInit(): void {
+    }
+
+    
+    // LIMPAR TELA DA CALCULADORA    
+    clean(){
+        this.calculadora = '';
+        this.resultado = 0;
+        this.sinal='';
+        this.valor = '';
+        this.numero = '';
+    }
+
+    Numero(valor){
+        this.calculadora = this.calculadora + valor;
+        this.numero = this.numero + valor;
+        console.log(this.numero);
+    }
+
+    Somar(){
+        this.valor = this.calculadora;
+        if(this.valor == ''){ //verifica se o usuário digitou algum número
+            this.calculadora = '';
+        }
+        else{
+            this.calculadora = this.valor + '+';
+            this.resultado = (this.resultado) + (parseInt(this.numero));
+            this.sinal = '+';
+            this.numero = '';
+            console.log(this.resultado);
+        }
+    }
+
+    Subtrair(){
+        this.valor = this.calculadora;
+        if(this.valor == ''){ //verifica se o usuário digitou algum número
+            this.calculadora = '';
+        }
+        else{
+            this.calculadora = this.valor + '-';
+            this.resultado = (parseInt(this.numero)) - (this.resultado);
+            this.sinal = '-';
+            this.numero = '';
+            console.log(this.resultado);
+        }
+    }
+
+    Multiplicar(){
+        this.valor = this.calculadora;
+        if(this.valor == ''){ //verifica se o usuário digitou algum número
+            this.calculadora = '';
+        }
+        else{
+            this.calculadora = this.valor + 'x';
+            this.resultado = parseInt(this.numero);
+            this.sinal = 'x';
+            this.numero = '';
+            console.log(this.resultado);
+        }
+    }
+
+    Resultado(){
+        if(this.sinal == '+'){
+            this.calculadora = String((this.resultado) + parseInt(this.numero));
+        }
+        if(this.sinal == '-'){
+            this.calculadora = String((this.resultado) - parseInt(this.numero));
+        }
+        if(this.sinal == 'x'){
+            this.calculadora = String((this.resultado) * parseInt(this.numero));   
+        }
+
+        //Permite que o resultado na tela seja usado para novas operações
+        this.numero = this.calculadora;
+        this.resultado = 0;
+    }
+    
+}
